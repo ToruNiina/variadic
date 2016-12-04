1da2d14e7f89dd3f96bbdd7205724ebd285844db
ToruNiina <niina.toru.68u@gmail.com>
2016-12-04 14:39:12 +0900

    change definition of rcompare and remove needless specialization

--- quicksort.hpp
+++ quicksort.hpp
@@ -1,6 +1,6 @@
 #ifndef VARIADIC_TEMPLATE_QUICK_SORT
 #define VARIADIC_TEMPLATE_QUICK_SORT
-#include "comprehension.hpp"
+#include "filter.hpp"
 
 template<template<typename T1, typename T2>class T_comp, typename ... Ts>
 struct quick_sort_impl;
@@ -9,23 +9,22 @@ template<template<typename T1, typename T2>class T_comp,
          typename T_pivot, typename ... Ts>
 struct quick_sort_impl<T_comp, pack<T_pivot, Ts...>>
 {
+    template<template<typename>class T_cond, typename T>
+    struct not_
+    {
+        constexpr static bool value = not T_cond<T>::value;
+    };
+
     template<typename T_lhs> using lcompare = T_comp<T_lhs, T_pivot>;
-    template<typename T_rhs> using rcompare = T_comp<T_pivot, T_rhs>;
+    template<typename T_rhs> using rcompare = not_<lcompare, T_rhs>;
 
     typedef typename connect<
-        typename quick_sort_impl<T_comp, 
-            typename comprehension<lcompare, Ts...>::type>::type,
+        typename quick_sort_impl<T_comp,
+            typename filter<lcompare, Ts...>::type>::type,
         typename push_front<T_pivot,
             typename quick_sort_impl<T_comp,
-                typename comprehension<rcompare, Ts...>::type>::type>::type
+                typename filter<rcompare, Ts...>::type>::type>::type
             >::type type;
-};
-
-template<template<typename T1, typename T2>class T_comp,
-         typename T>
-struct quick_sort_impl<T_comp, pack<T>>
-{
-    typedef pack<T> type;
 };
 
 template<template<typename T1, typename T2>class T_comp>
