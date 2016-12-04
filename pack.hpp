f58d96ab77f21b60f00e0cd47b710489949e6931
ToruNiina <niina.toru.68u@gmail.com>
2016-12-04 14:00:28 +0900

    add comments and rearrange the order

--- pack.hpp
+++ pack.hpp
@@ -4,8 +4,9 @@
 
 template<typename ... T> struct pack{};
 
-template<typename ... T>
-struct front;
+// element access
+
+template<typename ... T> struct front;
 
 template<typename T, typename ... Ts>
 struct front<pack<T, Ts...>>
@@ -28,66 +29,6 @@ struct back<pack<T>>
     typedef T type;
 };
 
-template<typename ... T>
-struct push_front;
-
-template<typename T, typename ... T_args>
-struct push_front<T, pack<T_args...>>
-{
-    typedef pack<T, T_args...> type;
-};
-
-template<typename ... T>
-struct push_back;
-
-template<typename T, typename ... T_args>
-struct push_back<T, pack<T_args...>>
-{
-    typedef pack<T_args..., T> type;
-};
-
-template<typename ... T>
-struct pop_front;
-
-template<typename T, typename ... T_args>
-struct pop_front<pack<T, T_args...>>
-{
-    typedef pack<T_args...> type;
-};
-
-template<typename ... T>
-struct pop_back;
-
-template<typename T, typename ... Ts>
-struct pop_back<pack<T, Ts...>>
-{
-    typedef typename push_front<T, typename pop_back<pack<Ts...>>::type>::type type;
-};
-
-template<typename T>
-struct pop_back<pack<T>>
-{
-    typedef pack<> type;
-};
-
-template<template<typename ... T>class target, typename ... T_args>
-struct transfer;
-
-template<template<typename ... T>class target, typename ... T_args>
-struct transfer<target, pack<T_args...>>
-{
-    typedef target<T_args...> type;
-};
-
-template<typename ... T_args>
-struct connect;
-
-template<typename ... T1, typename ... T2>
-struct connect<pack<T1...>, pack<T2...>>
-{
-    typedef pack<T1..., T2...> type;
-};
-
 template<std::size_t N, typename ... T_args>
 struct at;
 
@@ -102,6 +43,73 @@ template<typename T, typename ... T_args>
 struct at<0, pack<T, T_args...>>
 {
     typedef T type;
+};
+
+// insertion/deletion
+
+template<typename ... T>
+struct push_front;
+
+template<typename T, typename ... T_args>
+struct push_front<T, pack<T_args...>>
+{
+    typedef pack<T, T_args...> type;
+};
+
+template<typename ... T>
+struct pop_front;
+
+template<typename T, typename ... T_args>
+struct pop_front<pack<T, T_args...>>
+{
+    typedef pack<T_args...> type;
+};
+
+template<typename ... T>
+struct push_back;
+
+template<typename T, typename ... T_args>
+struct push_back<T, pack<T_args...>>
+{
+    typedef pack<T_args..., T> type;
+};
+
+template<typename ... T>
+struct pop_back;
+
+template<typename T, typename ... Ts>
+struct pop_back<pack<T, Ts...>>
+{
+    typedef typename push_front<
+        T, typename pop_back<pack<Ts...>>::type>::type type;
+};
+
+template<typename T>
+struct pop_back<pack<T>>
+{
+    typedef pack<> type;
+};
+
+// transfer to another struct
+
+template<template<typename ... T>class target, typename ... T_args>
+struct transfer;
+
+template<template<typename ... T>class target, typename ... T_args>
+struct transfer<target, pack<T_args...>>
+{
+    typedef target<T_args...> type;
+};
+
+// connect two different packs
+
+template<typename ... T_args>
+struct connect;
+
+template<typename ... T1, typename ... T2>
+struct connect<pack<T1...>, pack<T2...>>
+{
+    typedef pack<T1..., T2...> type;
 };
 
 #endif // VARIADIC_TEMPLATE_PACK
