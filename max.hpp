355561973990a93cf5fd4ea2bf8781f5deb4604d
ToruNiina <niina.toru.68u@gmail.com>
2016-12-04 14:14:03 +0900

    remove needless specialization

--- max.hpp
+++ max.hpp
@@ -2,10 +2,10 @@
 #define VARIADIC_TEMPLATE_MAX
 #include "pack.hpp"
 
-template<template<typename T1, typename T2>class T_larger, typename ... Ts>
+template<template<typename, typename>class T_larger, typename ... Ts>
 struct max_impl;
 
-template<template<typename U1, typename U2>class T_larger,
+template<template<typename, typename>class T_larger,
          typename T1, typename T2, typename ... Ts>
 struct max_impl<T_larger, pack<T1, T2, Ts...>>
 {
@@ -13,20 +13,13 @@ struct max_impl<T_larger, pack<T1, T2, Ts...>>
         T_larger, pack<typename T_larger<T1, T2>::type, Ts...>>::type type;
 };
 
-template<template<typename U1, typename U2>class T_larger,
-         typename T1, typename T2>
-struct max_impl<T_larger, pack<T1, T2>>
-{
-    typedef typename T_larger<T1, T2>::type type;
-};
-
-template<template<typename U1, typename U2>class T_larger, typename T>
+template<template<typename, typename>class T_larger, typename T>
 struct max_impl<T_larger, pack<T>>
 {
     typedef T type;
 };
 
-template<template<typename T1, typename T2>class T_larger, typename ... Ts>
+template<template<typename, typename>class T_larger, typename ... Ts>
 struct max
 {
     typedef typename max_impl<T_larger, pack<Ts...>>::type type;
