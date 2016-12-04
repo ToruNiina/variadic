4040da1166f0c8c04f9f9020d86b16a38ef24f59
ToruNiina <niina.toru.68u@gmail.com>
2016-12-04 14:24:11 +0900

    rename filter

--- test/filter.cpp
+++ test/filter.cpp
@@ -1,9 +1,11 @@
-#include "comprehension.hpp"
-
+#include "filter.hpp"
 #include <type_traits>
+
 static_assert(std::is_same<
-        comprehension<std::is_integral, char, int, double, float>::type,
+        filter<std::is_integral, char, int, double, float>::type,
         pack<char, int>>::value, "comprehension.is_integral");
+
+
 
 template<typename T1, typename T2>
 struct Greater : std::integral_constant<bool, (T1::value > T2::value)>{};
@@ -12,7 +14,7 @@ template<typename T>
 using greater5 = Greater<T, std::integral_constant<int, 5>>;
 
 static_assert(std::is_same<
-        comprehension<greater5, 
+        filter<greater5, 
             std::integral_constant<int, 5>,
             std::integral_constant<int, 1>,
             std::integral_constant<int, 9>,
