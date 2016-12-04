e838f21feefa70fe39cebdd36653be60470a0efe
ToruNiina <niina.toru.68u@gmail.com>
2016-12-04 14:19:28 +0900

    rename bifurcation to if_

--- test/max.cpp
+++ test/max.cpp
@@ -2,35 +2,19 @@
 #include <type_traits>
 #include <cstdint>
 
-template<bool B, typename T1, typename T2>
-struct bifurcation;
-template<typename T1, typename T2>
-struct bifurcation<true, T1, T2>
-{
-    typedef T1 type;
-};
-template<typename T1, typename T2>
-struct bifurcation<false, T1, T2>
-{
-    typedef T2 type;
-};
-
-template<typename T1, typename T2>
-struct Greater;
+template<bool B, typename T1, typename T2> struct if_;
+template<typename T1, typename T2> struct if_<true, T1, T2>{typedef T1 type;};
+template<typename T1, typename T2> struct if_<false, T1, T2>{typedef T2 type;};
 
 template<typename T1, typename T2>
 struct Greater
 {
-    typedef typename bifurcation<(sizeof(T1) > sizeof(T2)), T1, T2>::type type;
+    typedef typename if_<(sizeof(T1) > sizeof(T2)), T1, T2>::type type;
 };
 
 static_assert(std::is_same<
     typename max<Greater,
-            std::int8_t,
-            std::int16_t,
-            std::int32_t,
-            std::int64_t
-        >::type,
+        std::int8_t, std::int16_t, std::int32_t, std::int64_t>::type,
     std::int64_t
     >::value, "max");
 
